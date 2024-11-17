3c161927b7bfdb9ac0f83f0d0ef453af617ef770
Nguyen Duy <thanhduy1706@gmail.com>
2024-11-17 16:09:54 +0700

    Update Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,68 @@
 import WorkflowList from "./pages/WorkflowList"
-
 import DeadlinePopUp from "./components/Schedule/DeadlinePopUp"
 import NotePopUp from "./components/Schedule/NotePopUp"
 import NotificationPopUp from "./components/Schedule/NotificationPopUp"
-
 import Login from "./components/Login_Register/login.tsx"
 import Register from "./components/Login_Register/register.tsx"
-
 import Filters from "./components/Timetable/Filters.tsx"
-
 import ScheduleView from "./components/Schedule/ScheduleView"
 import Footer from "./components/Header_Footer/footer.tsx"
 import Header from "./components/Header_Footer/header.tsx"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route, Routes, Outlet } from "react-router-dom"
 
-import { BrowserRouter as Router, useRoutes } from "react-router-dom"
-
-function App() {
-  const AppRoutes = () =>
-    useRoutes([
-      { path: "/admin/WorkflowList", element: <WorkflowList /> },
-      { path: "/deadlinePopUp", element: <DeadlinePopUp /> },
-      { path: "/notePopUp", element: <NotePopUp /> },
-      { path: "/notificationPopUp", element: <NotificationPopUp /> },
-      { path: "/login", element: <Login /> },
-      { path: "/register", element: <Register /> },
-      { path: "/scheduleView", element: <ScheduleView /> },
-      { path: "/filters", element: <Filters /> },
-    ])
-
+const DefaultLayout = () => {
   return (
-    <div>
-      <Router>
-        <AppRoutes />
-        {window.location.pathname !== "/admin/WorkflowList" && <Header />}
-        {window.location.pathname !== "/admin/WorkflowList" && <Footer />}
-      </Router>
-    </div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   )
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/admin/WorkflowList"
+          element={<WorkflowList />}
+        />
+
+        <Route element={<DefaultLayout />}>
+          <Route
+            path="/"
+            element={<DeadlinePopUp />}
+          />
+          <Route
+            path="/notePopUp"
+            element={<NotePopUp />}
+          />
+          <Route
+            path="/notificationPopUp"
+            element={<NotificationPopUp />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+          <Route
+            path="/scheduleView"
+            element={<ScheduleView />}
+          />
+          <Route
+            path="/filters"
+            element={<Filters />}
+          />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
 export default App
